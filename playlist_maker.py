fec3b9fe5f9b67ed772ebb6f38c16ded91d68c29
venkat24 <venkatramansrikanth@outlook.com>
2019-06-19 06:56:32 +0530

    Add Album playlist
     - Collects tracks from all albums and uploads them to a separate playlist

--- playlist_maker.py
+++ playlist_maker.py
@@ -3,59 +3,91 @@ import re
 import sys
 import pprint
 
+from utils import *
 from config import *
 from youtube_client import YoutubePlaylistClient
 from spotify_client import SpotifyPlaylistClient
 
-# Open up the chats file
-if len(sys.argv) > 1:
-    chats_file_name = sys.argv[1]
-else:
-    print(f'Usage: {sys.argv[0]} chats.txt')
-    sys.exit()
+def add_to_spotify(urls):
+    spotify_track_urls = []
+    spotify_album_urls = []
+    for url in urls:
+        if "spotify" in url and "/track" in url:
+            new_url = url.split("?")[0]
+            spotify_track_urls.append(new_url)
+        
+        if "spotify" in url and "/album" in url:
+            new_url = url.split("?")[0]
+            spotify_album_urls.append(new_url)
 
-chats_file = open(chats_file_name, 'r')
-content = chats_file.read()
+    print(f'Found {len(spotify_track_urls)} Spotify track links. Uploading..')
 
-# Magic regex that extracts all URLs from the string
-urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', content)
+    spotify_client = SpotifyPlaylistClient()
 
-# Spotify
-spotify_urls = []
-for url in urls:
-    if "spotify" in url and "track" in url:
-        new_url = url.split("?")[0]
-        spotify_urls.append(new_url)
+    # Add standard tracks to the main playlist
+    spotify_client.replace_tracks_in_playlist(PLAYLIST_ID, spotify_track_urls)
 
-print(f'Found {len(spotify_urls)} Spotify links. Uploading to playlist now..')
+    # Add album tracks to the separate album playlist
+    all_album_tracks = []
+    for url in spotify_album_urls:
+        album_tracks = spotify_client.get_tracks_in_album(url)
+        all_album_tracks.extend(album_tracks)
 
-spotify_client = SpotifyPlaylistClient(PLAYLIST_ID)
-spotify_client.add_tracks_to_playlist(spotify_urls)
+    print(f'Found {len(spotify_album_urls)} albums with {len(all_album_tracks)} tracks. Uploading..')
 
-print("Uploaded successfully to Spotify!")
+    # Spotify has an upload limit of 100 tracks per request, so upload in chunks
+    for i, track_chunk in enumerate(chunks(all_album_tracks, 100)):
+        # Replace all tracks on the first chunk, append them on subsequent chunks
+        if i == 0:
+            spotify_client.replace_tracks_in_playlist(ALBUM_PLAYLIST_ID, track_chunk)
+        else:
+            spotify_client.add_tracks_to_playlist(ALBUM_PLAYLIST_ID, track_chunk)
 
-# YouTube
-youtube_urls = []
-for url in urls:
-    if "youtube" in url and "watch" in url:
-        new_url = url.split("?v=")[1]
-        youtube_urls.append(new_url)
+    print("Uploaded successfully to Spotify!")
 
-youtube_client = YoutubePlaylistClient(YT_PLAYLIST_ID)
+def add_to_youtube(urls):
+    youtube_urls = []
+    for url in urls:
+        if "youtube" in url and "watch" in url:
+            new_url = url.split("?v=")[1]
+            youtube_urls.append(new_url)
 
-print("Clearing existing playlist..")
-video_ids = youtube_client.get_all_playlist_items()
-for video in video_ids:
-    youtube_client.remove_video_from_playlist(video)
-print("Cleared existing playlist")
+    youtube_client = YoutubePlaylistClient(YT_PLAYLIST_ID)
 
-print(f'Found {len(youtube_urls)} YouTube links. Uploading to playlist now..')
+    print("Clearing existing playlist..")
+    video_ids = youtube_client.get_all_playlist_items()
+    for video in video_ids:
+        youtube_client.remove_video_from_playlist(video)
+    print("Cleared existing playlist")
 
-counter = 0
-for url in youtube_urls:
-    youtube_client.add_video_to_playlist(url)
-    counter += 1
-    print(f'Uploaded video {counter} of {len(youtube_urls)}..')
+    print(f'Found {len(youtube_urls)} YouTube links. Uploading to playlist now..')
 
-print("Uploaded successfully to YouTube!")
-print("My work is complete. Happy Listening!")
+    counter = 0
+    for url in youtube_urls:
+        try:
+            youtube_client.add_video_to_playlist(url)
+            counter += 1
+            print(f'Uploaded video {counter} of {len(youtube_urls)}..')
+        except Exception as e:
+            print(f'Video {url} FAILED due to {e}. Continuing..')
+
+    print("Uploaded tracks successfully to YouTube!")
+
+if __name__ == "__main__":
+    # Open up the chats file
+    if len(sys.argv) > 1:
+        chats_file_name = sys.argv[1]
+    else:
+        print(f'Usage: {sys.argv[0]} chats.txt')
+        sys.exit()
+
+    chats_file = open(chats_file_name, 'r')
+    content = chats_file.read()
+
+    # Magic regex that extracts all URLs from the string
+    urls = re.findall('(?:(?:https?|ftp):\/\/)?[\w/\-?=%.]+\.[\w/\-?=%.]+', content)
+
+    add_to_spotify(urls)
+    add_to_youtube(urls)
+
+    print("My work is complete. Happy Listening!")
--- spotify_client.py
+++ spotify_client.py
@@ -4,10 +4,9 @@ import spotipy.util as util
 from config import *
 
 class SpotifyPlaylistClient:
-    def __init__(self, playlist_id):
+    def __init__(self):
         scope = 'playlist-modify-public'
         token = util.prompt_for_user_token(USERNAME, scope, CLIENT_ID, CLIENT_SECRET, REDIRECT_URL)
-        self.playlist_id = playlist_id
 
         if token:
             self.sp = spotipy.Spotify(auth=token)
@@ -16,5 +15,12 @@ class SpotifyPlaylistClient:
             print(f'Can\'t get token for {USERNAME}')
             return None
 
-    def add_tracks_to_playlist(self, track_urls):
-        return self.sp.user_playlist_replace_tracks(USERNAME, self.playlist_id, track_urls)
+    def add_tracks_to_playlist(self, playlist_id, track_urls):
+        return self.sp.user_playlist_add_tracks(USERNAME, playlist_id, track_urls)
+
+    def replace_tracks_in_playlist(self, playlist_id, track_urls):
+        return self.sp.user_playlist_replace_tracks(USERNAME, playlist_id, track_urls)
+
+    def get_tracks_in_album(self, album_id):
+        tracks = self.sp.album_tracks(album_id)
+        return [track["id"] for track in tracks["items"]]
